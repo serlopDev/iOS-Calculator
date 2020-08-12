293c72d7855fbe10fbdf35b3a80d07755c7c82df
Sergio López <sergio.lofer.dev@gmail.com>
2020-08-12 10:51:35 +0200

    Subida de la base del proyecto a la rama Master

---
+++ iOS_Calculator/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iOS_Calculator
+//
+//  Created by Sergio López on 12/08/2020.
+//  Copyright © 2020 Sergio López. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
