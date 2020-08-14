e56290f325042585c66db0def46ced419e5ef20b
Sergio López <sergio.lofer.dev@gmail.com>
2020-08-14 11:02:01 +0200

    Creación de la vista y controlador Home // Creación de los botones en la vista y del visor de resultados

--- iOS_Calculator/AppDelegate.swift
+++ iOS_Calculator/AppDelegate.swift
@@ -14,8 +14,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Setup
+        
+        setupView()
+        
         return true
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupView(){
+        
+        // Instanciamos nuestro controlador Inicial
+        
+        let vc = HomeViewController()
+        
+        // Indicamos que será nuestra pantalla inicial y que esté visible
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 
 }
---
+++ iOS_Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  iOS_Calculator
+//
+//  Created by Sergio López on 12/08/2020.
+//  Copyright © 2020 Sergio López. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - Inicialización
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Ciclos de vida
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
